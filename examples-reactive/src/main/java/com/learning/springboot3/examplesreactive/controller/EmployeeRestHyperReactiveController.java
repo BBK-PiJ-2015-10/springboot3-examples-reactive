33940ef794de53b848a310accdd22f2d664eb8e6
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2024-08-12 10:13:17 -0400

    updating implementation with Thymeleaf

--- examples-reactive/src/main/java/com/learning/springboot3/examplesreactive/controller/EmployeeRestHyperReactiveController.java
+++ examples-reactive/src/main/java/com/learning/springboot3/examplesreactive/controller/EmployeeRestHyperReactiveController.java
@@ -10,7 +10,6 @@ import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
 
---
+++ examples-reactive/src/main/java/com/learning/springboot3/examplesreactive/controller/WebController.java
@@ -0,0 +1,44 @@
+package com.learning.springboot3.examplesreactive.controller;
+
+
+import com.learning.springboot3.examplesreactive.dto.EmployeeDto;
+import com.learning.springboot3.examplesreactive.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Controller
+public class WebController {
+
+    private EmployeeService employeeService;
+
+    public WebController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/")
+    public Mono<Rendering> index() {
+        return employeeService.getAll().collectList()
+                .map(employees ->
+                        Rendering.view("index")
+                                .modelAttribute("employees", employees)
+                                .modelAttribute("newEmployee",new EmployeeDto("",""))
+                                .build()
+                );
+
+    }
+
+    @PostMapping("/new-employee")
+    Mono<String> newEmployee(@ModelAttribute Mono<EmployeeDto> newEmployee) {
+        return newEmployee
+                .flatMap(e ->
+                        employeeService.save(e))
+                .map(e -> "redirect:/");
+    }
+
+}
